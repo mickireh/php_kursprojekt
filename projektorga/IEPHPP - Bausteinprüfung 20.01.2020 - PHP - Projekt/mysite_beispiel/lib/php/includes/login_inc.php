8749525eb7bc4f0b32bf144707397ef3cbfe69a8
Micki <gsmhirsch@gmail.com>
2020-06-19 19:52:50 +0200

    initial

--- projektorga/IEPHPP - Bausteinprüfung 20.01.2020 - PHP - Projekt/mysite_beispiel/lib/php/includes/login_inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if(isset($_SESSION['login']) && $_SESSION['login'] === 'ok') {
-        echo '<p>Sie sind als <strong>'.$_SESSION['username']. '</strong> eingeloggt.</p>';
-        echo '<form action="/kurs/javascript/mysite/lib/php/logout.php" method="post"><input type="submit" name="logout" value="Ausloggen"></form>';
-    } else {
-        echo '<form action="/kurs/javascript/mysite/lib/php/login.php" method="POST"><ul class="form-style-1"><li><label>Benutzername <span class="required">*</span></label><input type="text" name="user" id="user" class="field"></li><li><label>Passwort <span class="required">*</span></label><input type="password" name="password" id="password" class="field"></li><li><input type="submit" name="login" value="Submit"></li></ul></form>';
-    }
-?>
-
